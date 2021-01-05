65b9ab6a55c9edbd1078cba175c4d1a460966bee
dyaksa <diasnour0395@gmail.com>
2021-01-05 11:26:08 +0700

    create input and event

--- index.js
+++ index.js
@@ -3,17 +3,33 @@ const bodyParser = require("body-parser");
 const { graphqlHTTP } = require("express-graphql");
 const { buildSchema } = require("graphql");
 
+const events = []
+
 const app = express();
 app.use(bodyParser.json());
 
 app.use("/graphql",graphqlHTTP({
     schema: buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+        }
+
         type RootQuery {
-            events: [String!]
+            events: [Event!]!
         }
 
         type RootMutation {
-            createEvent(name: String): String
+            createEvent(eventInput: EventInput): Event
         }
 
         schema {
@@ -23,11 +39,18 @@ app.use("/graphql",graphqlHTTP({
     `),
     rootValue: {
         events: () => {
-            return ['Romantic','Sailing', 'All Night'];
+            return events;
         },
         createEvent: (args) => {
-            const eventName = args.name;
-            return eventName;
+            const event = {
+                _id: Math.random().toString(),
+                title: args.eventInput.title,
+                description: args.eventInput.description,
+                price: +args.eventInput.price,
+                date: new Date().toISOString()
+            }
+            events.push(event);
+            return event;
         }
     },
     graphiql: true
