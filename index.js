e89a98b4c29761f6a6a2307c16fb11440474b437
dyaksa <diasnour0395@gmail.com>
2021-01-06 00:01:48 +0700

    relational mongodb

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { graphqlHTTP } = require("express-graphql");
 const { buildSchema } = require("graphql");
+const bcrypt = require("bcryptjs");
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+require("dotenv").config();
 
-const events = []
+//schema
+const Event = require("./models/event");
+const User = require("./models/user");
 
 const app = express();
 app.use(bodyParser.json());
@@ -18,10 +24,21 @@ app.use("/graphql",graphqlHTTP({
             date: String!
         }
 
+        type User {
+            _id: ID!
+            email: String!
+            password: String
+        }
+
         input EventInput {
             title: String!
             description: String!
             price: Float!
+        }
+
+        input UserInput {
+            email: String!
+            password: String!
         }
 
         type RootQuery {
@@ -30,6 +47,7 @@ app.use("/graphql",graphqlHTTP({
 
         type RootMutation {
             createEvent(eventInput: EventInput): Event
+            createUser(userInput: UserInput): User
         }
 
         schema {
@@ -39,23 +57,74 @@ app.use("/graphql",graphqlHTTP({
     `),
     rootValue: {
         events: () => {
-            return events;
+            return Event.find((err,events) => {
+                if(!err){
+                    return events.map((event) => {
+                        const results = {...event._doc, _id: event.id.toString(), date: event.date.toISOString()};
+                        return results;
+                    })
+                }
+                throw err;
+            })
         },
         createEvent: (args) => {
-            const event = {
-                _id: Math.random().toString(),
+            let createdEvent;
+            const event = new Event({
                 title: args.eventInput.title,
                 description: args.eventInput.description,
-                price: +args.eventInput.price,
-                date: new Date().toISOString()
-            }
-            events.push(event);
-            return event;
+                price: args.eventInput.price,
+                date: new Date(),
+                creator: "5ff495dbba3ee9021825c69b"
+            })
+            return event
+            .save()
+            .then(result => {
+                createdEvent =  {...result._doc, _id: result._doc._id.toString()}
+                return User.findById("5ff495dbba3ee9021825c69b")
+            }).then(user => {
+                if(!user){
+                    throw new Error("User not exists");
+                }
+                user.createdEvents.push(event);
+                return user.save();
+            })
+            .then(() => {
+                return createdEvent;
+            }).catch(err => {
+                throw err;
+            })
+        },
+        createUser: (args) => {
+            return User.findOne({email: args.userInput.email})
+            .then(user => {
+                if(user){
+                    throw new Error("user already exists");
+                }
+                return bcrypt.hash(args.userInput.password,12)
+            })
+            .then((hash) => {
+                const user = new User({
+                    email: args.userInput.email,
+                    password: hash
+                })
+                return user.save();
+            }).then(user => {
+                return {...user._doc, _id: user.id.toString(), password: null};
+            })
+            .catch(err => {
+                throw err;
+            })
         }
     },
     graphiql: true
 }))
 
-app.listen(process.env.PORT || 8000, () => {
-    console.log(`server running on port 8000`);
+mongoose.connect(`
+mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.ckvwl.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
+{useNewUrlParser: true, useUnifiedTopology: true}
+).then(() => {
+    app.listen(8000);
+    console.log("server is running");
+}).catch(err => {
+    throw err
 })
