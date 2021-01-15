4875b62b6108610129f4014a0bcc3277d35ec308
dyaksa <diasnour0395@gmail.com>
2021-01-15 09:55:58 +0700

    setup react router

---
+++ frontend/src/pages/Auth/index.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+class Auth extends Component {
+    render(){
+        return (
+            <h1>this Auth Page</h1>
+        )
+    }
+}
+
+export default Auth;
---
+++ frontend/src/pages/Bookings/index.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+class Bookings extends Component {
+    render(){
+        return(
+            <h1>This is Bookings</h1>
+        )
+    }
+}
+
+export default Bookings;
