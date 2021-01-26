ce7bbd119ca7b934e25581da14006f36f9b7c5b7
dyaksa <diasnour0395@gmail.com>
2021-01-26 11:21:09 +0700

    adding package react hook form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Router>
         <>
         <MainNavigation/>
-        <main>
+        <main className="main-content">
           <Switch>
             <Redirect from="/" to="/auth" exact={true}/>
             {routes.map((val,key) => (
--- frontend/src/pages/Auth/index.js
+++ frontend/src/pages/Auth/index.js
@@ -1,9 +1,37 @@
 import { Component } from "react";
+import "./Auth.css";
 
 class Auth extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            email: null,
+            password: null
+        }
+    }
+
+    submitHandler = (event) => {
+        event.preventDefault();
+        console.log(this.state);
+    }
+
     render(){
         return (
-            <h1>this Auth Page</h1>
+            <form className="form-auth" onSubmit={this.submitHandler}>
+                <div className="form-control">
+                    <label htmlFor="email">Email</label>
+                    <input onChange={(e) => this.setState({email: e.target.value}) } type="email" id="email"></input>
+                </div>
+                <div className="form-control">
+                    <label htmlFor="password">Password</label>
+                    <input onChange={(e) => this.setState({password: e.target.value})} type="password" id="password"></input>
+                </div>
+                <div className="form-actions">
+                    <button type="submit">Submit</button>
+                    <button type="button">Switch to Signup</button>
+                </div>
+            </form>
         )
     }
 }
