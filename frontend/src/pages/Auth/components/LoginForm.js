d95fa1ea12810488dc85185928164d6ddad42c2a
dyaksa <diasnour0395@gmail.com>
2021-01-27 13:41:25 +0700

    authentication method

--- frontend/src/pages/Auth/components/LoginForm.js
+++ frontend/src/pages/Auth/components/LoginForm.js
@@ -1,12 +1,53 @@
 import "./LoginForm.css";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import Input from "../../../components/Input";
+import Button from "../../../components/Button";
 
 const LoginForm = (props) => {
     const { register, handleSubmit, errors } = useForm();
+    const [isLogin,setIsLogin] = useState(false);
+
+    const switchHandler = () => setIsLogin(!isLogin);
 
     const onSubmit = (data) => {
-        console.log(data);
+        const { email,password } = data;
+        let requestBody = {
+            query: `
+                query {
+                    login(email: "${email}", password: "${password}"){
+                        _id
+                        token
+                        expiredIn
+                    }
+                }`
+            }
+        if(!isLogin){
+            requestBody = {
+                query: `
+                    mutation {
+                        createUser(userInput: {email: "${email}", password: "${password}"}){
+                            _id
+                            email
+                            password
+                        }
+                    }`
+                }
+            }
+
+        fetch('http://localhost:8000/graphql',{
+            method: "POST",
+            body: JSON.stringify(requestBody),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(res => {
+            return res.json();
+        }).then(resBody => {
+            console.log(resBody);
+        }).catch(err => {
+            console.log(err)
+        })
     }
 
     return (
@@ -14,8 +55,8 @@ const LoginForm = (props) => {
             <Input error={(errors.email) ? true : false} validate={register({required: true})} name="email" label="Email Address" type="email"/>
             <Input error={(errors.password) ? true : false} validate={register({required: true})} name="password" label="Password" type="password"/>
             <div className="form-actions">
-                <button type="submit">Submit</button>
-                <button type="button">Switch to Signup</button>
+                <Button type="submit" title="Submit"/>
+                <Button click={switchHandler} type="button" title={(!isLogin) ? 'Switch to Login' : 'Switch to Signup'}/>
             </div>
         </form>
     )
